8f86d00a5581e07b98b7f89c5e83d49ad8c3ce0f
Lucas Oliveira <lucasoliveiralog@hotmail.com>
2023-07-25 19:50:29 -0300

    Conexão com API cyclic

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://backend-equipe6.cyclic.app',
   timeout: 10000,
   headers: { 'Content-type': 'Application/json' }
 });
